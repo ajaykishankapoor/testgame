86c46c9a77ae234c3b1c84c2b1fe5e75ecb7c3d1
ajaykishankapoor <60594507+ajaykishankapoor@users.noreply.github.com>
2020-02-03 12:24:38 +0530

    Update config.js

--- registrationlogin/config.js
+++ registrationlogin/config.js
@@ -1,12 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'letslin1_ajay',
-  password : 'Slayer82!',
-  database : 'letslin1_ajay'
+  // xxxx
 });
 
-//$db = mysqli_connect('localhost', 'letslin1_ajay', 'Slayer82!', 'letslin1_ajay');
+
 
 connection.connect(function(err){
 if(!err) {
